fc2214661ae023429d3d9741be5e3ffba61c490b
Haoqi Shi <haoqi.shi@xyzrobotics.com>
2024-06-07 11:10:54 +0800

    add align axis & pickable

---
+++ infinigen/assets/robots/motion.py
@@ -0,0 +1,87 @@
+import json
+import tempfile
+import numpy as np
+import subprocess
+from mathutils import Matrix
+from subprocess import PIPE
+
+
+def check_executable(executable):
+    try:
+        result = subprocess.run(["which", executable], stdout=PIPE, stderr=PIPE)
+        if result.returncode == 0:
+            return True
+        else:
+            return False
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return False
+
+
+def write_cube_json(filepath, cubes):
+    data = []
+    for cube in cubes:
+        tf = np.array(cube.matrix_world.normalized()).astype(float)
+        size = np.array(cube.scale).astype(float)
+        data.append({"tf": tf.tolist(), "size": size.tolist()})
+    with open(filepath, "w") as f:
+        json.dump(data, f)
+
+
+def align_axis(cubes):
+    if not check_executable("align_axis"):
+        raise Exception("align_axis executable not found")
+
+    # create temporary directory
+    temp_dir = tempfile.mkdtemp()
+
+    input_file = f"{temp_dir}/boxes.json"
+    output_file = f"{temp_dir}/aligned.json"
+
+    # create boxes.json
+    write_cube_json(input_file, cubes)
+
+    # run align_axis
+    subprocess.run(
+        ["align_axis", "-i", input_file, "-o", output_file],
+        stdout=PIPE,
+        stderr=PIPE,
+    )
+
+    # read aligned.json
+    with open(output_file, "r") as f:
+        aligned_data = json.load(f)
+
+    # apply transform
+    for item, cube in zip(aligned_data, cubes):
+        cube.matrix_world = Matrix(np.array(item["tf"]))
+        cube.scale = item["size"]
+
+    return cubes
+
+
+def pickable(cubes):
+    if not check_executable("pickable"):
+        raise Exception("pickable executable not found")
+
+    # create temporary directory
+    temp_dir = tempfile.mkdtemp()
+
+    input_file = f"{temp_dir}/boxes.json"
+    output_file = f"{temp_dir}/pickable.json"
+
+    # create boxes.json
+    write_cube_json(input_file, cubes)
+
+    # run pickable
+    subprocess.run(
+        ["pickable", "-i", input_file, "-o", output_file],
+        stdout=PIPE,
+        stderr=PIPE,
+    )
+
+    # read aligned.json
+    with open(output_file, "r") as f:
+        pickable = json.load(f)
+
+    return pickable
--- infinigen_examples/create_world.py
+++ infinigen_examples/create_world.py
@@ -4,14 +4,28 @@ import numpy as np
 import infinigen.core.util.blender as butil
 from mathutils import Matrix
 
+
+def load_cube_json(filepath):
+    cubes = []
+    with open(filepath, "r") as f:
+        data = json.load(f)
+        for item in data:
+            cube = butil.spawn_cube(size=1)
+            cube.matrix_world = Matrix(np.array(item["tf"]))
+            cube.scale = item["size"]
+            cubes.append(cube)
+    return cubes
+
+
 butil.clear_scene()
 
-filepath = "/home/shq/Downloads/test_symmetry/test/sym24.json"
-with open(filepath, "r") as f:
-    data = json.load(f)
-    for item in data:
-        cube = butil.spawn_cube(size=1)
-        cube.matrix_world = Matrix(np.array(item["tf"]))
-        cube.scale = item["size"]
+cubes = load_cube_json(
+    "/home/shq/Projects/mycode/resolve_collision/data/pickable/boxes.json"
+)
+butil.group_in_collection(cubes, "GT World")
 
-butil.save_blend('output/test.blend')
+cubes = load_cube_json("/home/shq/Projects/mycode/resolve_collision/output/output.json")
+butil.group_in_collection(cubes, "Pickable")
+
+
+butil.save_blend("output/test.blend")
